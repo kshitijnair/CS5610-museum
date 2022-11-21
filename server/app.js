78f214100980234256f9b020a6fbecff79d36bc0
unknown <kshitijnairr@gmail.com>
2022-11-21 11:53:30 -0800

    added databse connection succesfully

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const route = express.Router();
 const axios = require("axios");
 
+const {connectToDB} = require('./database');
+
 const app = express();
-const PORT = 8000;
+const PORT = 3000;
 
 const homeRouter = require("./routes/home");
 const profileRouter = require("./routes/profile");
@@ -11,12 +12,21 @@ const detailsRouter = require("./routes/details");
 const loginRouter = require("./routes/login");
 const searchRouter = require("./routes/search");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use("/home", homeRouter);
 app.use("/profile", profileRouter);
 app.use("/details", detailsRouter);
 app.use("/login", loginRouter);
 app.use("/search", searchRouter);
 
+
+app.get('/', (req, res) => {
+
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  connectToDB();
 });
