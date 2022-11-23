7f6e0c2fb560b9ff6b98e5f8ac0a7e213bdb50b1
ratnakirti <email.navneetr@gmail.com>
2022-11-23 15:25:59 -0800

    Implemented GET, POST and DELETE user in the Profile router

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-
-const { getProfile } = require('../database')
-
-const database = require('../database');
+const { ObjectId } = require("mongodb");
+const {
+  getProfile,
+  getFirstUser,
+  removeUser,
+  editUser,
+} = require("../database");
 
 router.get("/", (req, res) => {
   res.send("Hello ProfilePage");
 });
 
 router.get("/user/:userID", async (req, res) => {
-  console.log("user id is: ", req.params.userID)
+  console.log("user id is: ", req.params.userID);
   const user = await getProfile("oops");
   res.json(user);
-})
+});
 
 router.post("/user/newuser", async (req, res) => {
   console.log("adding new user...");
+});
 
+router.get("/getFirstUser", async (req, res) => {
+  const user = await getFirstUser();
+  res.json(user);
+});
 
-})
+router.delete("/deleteUser", async (req, res) => {
+  console.log("deleting item");
+  const id = req.query["id"];
+  console.log(id);
+  let result = await removeUser({ _id: ObjectId(id) });
+  console.log(result);
+  res.json(result);
+});
+
+router.post("/updateUser", async (req, res) => {
+  const updatedData = {
+    name: `${req.body.name}`,
+    email: `${req.body.email}`,
+    phone: `${req.body.phone}`,
+  };
+  console.log(updatedData, req.body.id);
+  console.log("**************");
+  let response = await editUser({ _id: ObjectId(req.body.id) }, updatedData);
+  console.log(response);
+  res.json(response);
+});
 
 module.exports = router;
