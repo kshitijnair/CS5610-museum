f38de3e22faed26a3b3952c5fe0f7be23da0a0ea
ratnakirti <email.navneetr@gmail.com>
2022-11-27 00:13:40 -0800

    Added react navigation routers for Details and Search page

--- client/museum/src/App.js
+++ client/museum/src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { getUser, removeUser, updateUser, getMuseums } from './methods'
+import logo from "./logo.svg";
+import "./App.css";
+import { getUser, removeUser, updateUser, getMuseums } from "./methods";
 
-import Navigation from './components/Navigation';
-import Header from './components/Header';
-import UserComponent from './components/UserComponent';
-import MuseumComponent from './components/MuseumComponent';
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import UserComponent from "./components/UserComponent";
+import MuseumComponent from "./components/MuseumComponent";
+import Details from "./components/Details";
+import Search from "./components/Search";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Museum </h1>
-      <Navigation name='Kshitij'/> 
-      <Header title = "Users"/>
-      <UserComponent />
-      <MuseumComponent />
+  console.log(window.location);
 
-    </div>
+  return (
+    <>
+      <Navigation></Navigation>
+      <div className="container">
+        <h1>Museum </h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+
+        <Header title="Users" />
+        <UserComponent />
+        <MuseumComponent />
+      </div>
+    </>
   );
 }
 
-export default App; 
+export default App;
 
 /**
  * users - current user, update(form), delete
--- client/museum/src/components/Navigation.js
+++ client/museum/src/components/Navigation.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
-const Navigation = ({name}) => {
+const Navigation = ({ name }) => {
   return (
-    <div>Welcome to Navigation, {name}!</div>
-  )
+    <div>
+      <div>Welcome to Navigation, {name}!</div>
+      <nav className="nav">
+        <Link to="/" className="site-title">
+          INTERNET MUSEUM DATABASE
+        </Link>
+        <ul>
+          <CustomLink to="/search">Search</CustomLink>
+          <CustomLink to="/details">Details</CustomLink>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
 }
 
-export default Navigation
+export default Navigation;
